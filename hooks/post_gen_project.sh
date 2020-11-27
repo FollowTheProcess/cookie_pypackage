2d033e59f774494d5e0fcc1b1c27c3c052f6c0d9
Tom Fleet <tomfleet2018@gmail.com>
2020-11-27 10:42:19 +0000

    Fixed gh cli script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 if ["{{cookiecutter.use_gh_cli_to_create_repo}}" = "y"]; then
-    gh repo create {{cookiecutter.github_username}}/{{cookiecutter.project_slug}}
+    gh repo create {{cookiecutter.author_github_username}}/{{cookiecutter.project_slug}}
 fi
