9914e5b7ffdeb2f26e395da05e4eab88db19d4c7
Tom Fleet <tomfleet2018@gmail.com>
2020-11-27 10:38:38 +0000

    Fix dumb error with brackets

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-if ["{{cookiecutter.use_gh_cli_to_create_repo" = "y"]; then
+if ["{{cookiecutter.use_gh_cli_to_create_repo}}" = "y"]; then
     gh repo create {{cookiecutter.github_username}}/{{cookiecutter.project_slug}}
 fi
