b2d0bd4473a51f88b85f176d2e9b9e74ba820b92
Tom Fleet <tomfleet2018@gmail.com>
2020-11-27 10:45:52 +0000

    Fix github script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
-if ["{{cookiecutter.use_gh_cli_to_create_repo}}" = "y"]; then
-    gh repo create {{cookiecutter.author_github_username}}/{{cookiecutter.project_slug}}
-fi
+
+{% if cookiecutter.use_gh_cli_to_create_repo == 'y' -%}
+
+gh repo create {{cookiecutter.author_github_username}}/{{cookiecutter.project_slug}}
+
+{% endif %}
